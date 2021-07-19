e2c2612934c17b2bf5fb30dc81f5cbd960ac7739
Dave Mount <davemount@gmail.com>
2021-07-18 21:45:56 -0700

    Updating the value instead of replacing

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,5 @@
 <template>
   <q-page class="flex flex-center">
-    <div>Found {{total.count}} articles.</div>
     <div class="q-pa-md">
       <div class="row q-gutter-md">
         <div class="col-sm">
@@ -23,7 +22,8 @@
               <div class="text-subtitle2">by Zanzibar</div>
             </q-card-section>
             <q-card-section class="q-pt-none">
-              Everything you need to know about the parts of a nuclear power plant.
+              Everything you need to know about the parts of a nuclear power
+              plant.
             </q-card-section>
           </q-card>
         </div>
@@ -35,7 +35,20 @@
               <div class="text-subtitle2">by Zanzibar</div>
             </q-card-section>
             <q-card-section class="q-pt-none">
-              Find out about Gen IV nuclear power, how it avoids disaster and consumes nuclear waste.
+              Find out about Gen IV nuclear power, how it avoids disaster and
+              consumes nuclear waste.
+            </q-card-section>
+          </q-card>
+        </div>
+        <div v-for="article in articles" :key="article.id" class="col-sm">
+          <q-card class="my-card">
+            <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+            <q-card-section>
+              <div class="text-h6">{{ article.headline }}</div>
+              <div class="text-subtitle2">by {{ article.byline }}</div>
+            </q-card-section>
+            <q-card-section class="q-pt-none">
+              {{ article.content }}
             </q-card-section>
           </q-card>
         </div>
@@ -45,24 +58,25 @@
 </template>
 
 <script>
-import { defineComponent, onMounted, reactive } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
 import { fetchArticles } from '../api/PowerUpService'
 
 export default defineComponent({
   name: 'PageIndex',
   setup() {
-    let total = reactive({count:0})
+    let articles = ref([])
     const getArticles = async () => {
       const results = await fetchArticles()
-      total = {count: results.data.length}
+      results.data.forEach((article) => articles.value.push(article))
     }
 
     onMounted(getArticles)
 
     return {
-      total,
+      articles,
       lorem:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      getArticles,
     }
   },
 })
