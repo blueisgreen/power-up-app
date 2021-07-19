d656db1fcca27f9827948fe3fbe7e1d519138328
Dave Mount <davemount@gmail.com>
2021-07-18 20:46:52 -0700

    Set up cors

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require('express'),
-  serveStatic = require('serve-static'),
-  history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000
+const express = require('express')
+const serveStatic = require('serve-static')
+const history = require('connect-history-api-fallback')
+const cors = require('cors')
 
+const port = process.env.PORT || 5000
 const app = express()
 
+app.use(cors())
 app.use(history())
 app.use(serveStatic(__dirname + '/dist/spa'))
-app.listen(port)
+app.listen(port, () => {
+  console.log('Express service listening on port:' + port);
+})
