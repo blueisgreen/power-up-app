d28e184833e8310d2145c167d9fa614bd7182760
Dave Mount <davemount@gmail.com>
2021-07-22 11:15:39 -0700

    Sketched out user account page

---
+++ src/pages/MemberProfilePage.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-page>
+    <h3>Account Settings</h3>
+    <div>
+      <q-splitter v-model="splitterModel" style="height: 250px">
+        <template v-slot:before>
+          <q-tabs v-model="tab" vertical align="left">
+            <q-tab name="overview" icon="private" label="Overview"></q-tab>
+            <q-tab name="profile" icon="person" label="Profile"></q-tab>
+            <q-tab name="settings" icon="control" label="Preferences"></q-tab>
+          </q-tabs>
+        </template>
+        <template v-slot:after>
+          <q-tab-panels v-model="tab" vertical>
+            <q-tab-panel name="overview">
+              <h4>Overview</h4>
+              <p>
+                A read-only display of all settings, user history, and as on.
+              </p>
+            </q-tab-panel>
+            <q-tab-panel name="profile">
+              <h4>Member Profile</h4>
+              <p>
+                Form for changing profile information - what is shared with the
+                world.
+              </p>
+            </q-tab-panel>
+            <q-tab-panel name="settings">
+              <h4>Preferences</h4>
+              <p>Form for changing preferences - customize your experience.</p>
+            </q-tab-panel>
+          </q-tab-panels>
+        </template>
+      </q-splitter>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'ComingSoon',
+  data() {
+    return {
+      tab: ref('overview'),
+      splitterModel: ref(20),
+    }
+  },
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,13 @@
 
+import Account from 'pages/MemberProfilePage'
+
 const routes = [
   {
-    path: '/',
+    path: '/',  
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
-      { path: 'profile', component: () => import('pages/ComingSoon.vue') },
+      { path: 'profile', component: () => import('pages/MemberProfilePage.vue') },
       { path: 'learn', component: () => import('pages/ComingSoon.vue') },
       { path: 'boards', component: () => import('pages/ComingSoon.vue') },
       { path: 'simulation', component: () => import('pages/ComingSoon.vue') },
