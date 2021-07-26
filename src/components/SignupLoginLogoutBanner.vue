c751f4932bedd1c906c24e63869e6e2398104227
Dave Mount <davemount@gmail.com>
2021-07-25 18:50:27 -0700

    This does not work

--- src/components/SignupLoginLogoutBanner.vue
+++ src/components/SignupLoginLogoutBanner.vue
@@ -3,7 +3,12 @@
     <q-banner rounded class="bg-purple-8 text-white">
       Who are you?
       <template v-slot:action>
-        <q-btn flat color="white" label="Sign Up or Sign In" to="http://localhost:3000/login/github" />
+        <q-btn
+          flat
+          color="white"
+          label="Sign Up or Sign In"
+          to="{ path: 'http://localhost:3000/login/github', exact: true }"
+        />
         <q-btn flat color="white" label="Sign Out" />
       </template>
     </q-banner>
