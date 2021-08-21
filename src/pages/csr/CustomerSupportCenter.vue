8e7099b29f31a7788fdf55b71fd1828c2a50c305
Dave Mount <davemount@gmail.com>
2021-08-21 07:53:25 -0700

    Set up CSR page

---
+++ src/pages/csr/CustomerSupportCenter.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-md">
+    <h3>Customer Support Center</h3>
+    <p>
+      This is where administrator can see feedback and answer user inquiries.
+      Maybe the feedback should be sent by email.
+    </p>
+  </q-page>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
