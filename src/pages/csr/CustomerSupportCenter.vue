7eaef74f1ce14073b665898660dcf013d207c212
Dave Mount <davemount@gmail.com>
2021-08-21 11:59:22 -0700

    Add message area to view, respond, and see history

--- src/pages/csr/CustomerSupportCenter.vue
+++ src/pages/csr/CustomerSupportCenter.vue
@@ -2,12 +2,6 @@
   <q-page class="q-pa-md">
     <h3>Customer Support Center</h3>
     <q-toolbar class="bg-primary text-white q-my-md shadow-2">
-      <q-btn flat round dense icon="menu" class="q-mr-sm" />
-      <q-separator dark vertical inset />
-      <q-btn stretch flat label="Link" />
-
-      <q-space />
-
       <q-btn-dropdown stretch flat no-wrap label="Filter">
         <q-list>
           <q-item-label header>By Purpose</q-item-label>
@@ -56,8 +50,28 @@
           </q-item>
         </q-list>
       </q-btn-dropdown>
+
       <q-separator dark vertical />
-      <q-btn stretch flat label="Link" />
+      <q-space />
+
+      <q-input
+        v-model="searchQuery"
+        dark
+        dense
+        standout
+        input-class="text-right"
+        class="q-ml-md"
+      >
+        <template #append>
+          <q-icon v-if="searchQuery === ''" name="search" />
+          <q-icon
+            v-else
+            name="clear"
+            class="cursor-pointer"
+            @click="searchQuery = ''"
+          />
+        </template>
+      </q-input>
       <q-separator dark vertical />
       <q-btn stretch flat label="Link" />
     </q-toolbar>
@@ -74,22 +88,39 @@
             <q-item-label><q-avatar icon="help" /></q-item-label>
           </q-item-section>
           <q-item-section>
-            <q-item-label lines="3"
-              >If the birds and the bees are as smart as the tree, what color
-              are the dandilions in the springtime under a full moon? Also, what
-              is your favorite color? Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit, sed do eiusmod <b>tempor</b> incididunt ut labore
-              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-              occaecat cupidatat non proident, sunt in culpa qui officia
-              deserunt mollit anim id est laborum.</q-item-label
-            >
+            <q-item-label lines="3">{{ sampleMessage }}</q-item-label>
           </q-item-section>
         </q-item>
       </q-list>
     </q-scroll-area>
+    <q-card>
+      <q-tabs
+        v-model="inquiryTab"
+        inline-label
+        class="bg-purple text-white shadow-2"
+      >
+        <q-tab name="read" icon="mail" label="Read" />
+        <q-tab name="respond" icon="send" label="Respond" />
+        <q-tab name="history" icon="clock" label="History" />
+      </q-tabs>
+      <q-separator />
+      <q-tab-panels v-model="inquiryTab">
+        <q-tab-panel name="read">
+          <div>{{ sampleMessage }}</div>
+        </q-tab-panel>
+      </q-tab-panels>
+      <q-tab-panels v-model="inquiryTab">
+        <q-tab-panel name="respond">
+          <q-input v-model="sampleResponse" type="textarea"></q-input>
+          <q-btn color="primary">Submit Response</q-btn>
+        </q-tab-panel>
+      </q-tab-panels>
+      <q-tab-panels v-model="inquiryTab">
+        <q-tab-panel name="history">
+          <div>See other inquiries from the same user.</div>
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </q-page>
 </template>
 
@@ -98,8 +129,16 @@ import { defineComponent, ref } from 'vue'
 export default defineComponent({
   name: 'PageCustomerSupportCenter',
   setup() {
+    const sampleMessage =
+      'If the birds and the bees are as smart as the tree, what color are the dandilions in the springtime under a full moon? Also, what is your favorite color? Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <b>tempor</b> incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    const sampleResponse =
+      'Dear Blargy Pants, You will have to learn to deal with it, whatever your problem is. Sorry, life is hard.'
     return {
       showOnlyUnread: ref(false),
+      searchQuery: ref(''),
+      inquiryTab: ref('read'),
+      sampleMessage,
+      sampleResponse: ref(sampleResponse),
     }
   },
 })
@@ -107,7 +146,7 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .inquiry-list {
-  height: 500px;
+  height: 200px;
   max-width: 100%;
 }
 </style>
