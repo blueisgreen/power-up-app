c58f14163a350bcf3425923a62c06135f4c9e335
Dave Mount <davemount@gmail.com>
2021-11-23 07:47:38 -0800

    Sign in button handles multiple ID providers

--- src/components/AuthorizationWidget.vue
+++ src/components/AuthorizationWidget.vue
@@ -1,7 +1,28 @@
 <template>
   <div>
-    <q-btn v-show="!isSignedIn" @click="handleSignIn">Sign In</q-btn>
+    <!-- <q-btn v-show="!isSignedIn" @click="() => handleSignIn('github')">Sign In</q-btn> -->
+    <q-btn-dropdown v-show="!isSignedIn" color="primary" label="Sign In">
+      <q-list>
+        <q-item v-close-popup clickable @click="() => handleSignIn('github')">
+          <q-item-section>
+            <q-item-label>with GitHub</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item v-close-popup clickable @click="() => handleSignIn('google')">
+          <q-item-section>
+            <q-item-label>with Google</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item v-close-popup clickable @click="() => handleSignIn('linkedin')">
+          <q-item-section>
+            <q-item-label>with LinkedIn</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
     <q-btn v-show="isSignedIn" @click="handleSignOut">Sign Out</q-btn>
+  </div>
+  <div class="q-pa-md">
   </div>
 </template>
 
@@ -23,8 +44,8 @@ export default {
     handleSignOut() {
       this.store.dispatch('auth/signOutUser')
     },
-    handleSignIn() {
-      window.location.href = process.env.API_URL + '/login?pid=github'
+    handleSignIn(pid) {
+      window.location.href = `${process.env.API_URL}/login?pid=${pid}`
     },
   },
 }
