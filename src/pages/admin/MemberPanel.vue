7917326591884a71d0de8c1715e86b1cc733603c
Dave Mount <davemount@gmail.com>
2022-01-04 15:43:09 -0800

    Nicer looking list of members

--- src/pages/admin/MemberPanel.vue
+++ src/pages/admin/MemberPanel.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="q-pa-md">
     <h3>Member Administration</h3>
-    <ul>
-      <li v-for="user in users" :key="user.userKey">{{user.alias}} : {{user.userKey}}</li>
-    </ul>
+    <q-list>
+      <q-item v-for="user in users" :key="user.userKey" clickable>
+        <q-item-section avatar>
+          <img :src="user.avatarUrl">
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ user.alias }}</q-item-label>
+          <q-item-label caption lines="2">{{ user.userKey }}</q-item-label>
+        </q-item-section>
+        <q-item-section side top>
+          <q-item-label caption>{{ user.email }}</q-item-label>
+          <q-icon name="star" color="yellow" />
+        </q-item-section>
+      </q-item>
+    </q-list>
   </div>
 </template>
 
 <script>
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent, onMounted, ref} from 'vue'
 import { useStore, mapGetters } from 'vuex'
 
 export default defineComponent({
@@ -17,6 +29,9 @@ export default defineComponent({
     onMounted(() => {
       store.dispatch('admin/refreshUsers')
     })
+    return {
+      showDetail: false
+    }
   },
   computed: {
     ...mapGetters('admin', ['users'])
