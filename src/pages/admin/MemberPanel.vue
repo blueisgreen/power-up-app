4f576ea2232d04bfa544f145b1925e10be121a2e
Dave Mount <davemount@gmail.com>
2022-01-06 10:40:05 -0500

    Display assigned roles

--- src/pages/admin/MemberPanel.vue
+++ src/pages/admin/MemberPanel.vue
@@ -4,33 +4,58 @@
     <q-toolbar class="text-primary">
       <q-toolbar-title>
         Find Member
-      <q-input v-model="aliasToFind" label="Alias" stack-label />
+        <q-input v-model="searchQuery" label="Alias" stack-label />
       </q-toolbar-title>
       <q-btn flat round dense icon="fas fa-search" @click="search" />
     </q-toolbar>
 
     <q-list>
-      <q-item v-for="user in users" :key="user.userKey" clickable>
+      <q-item
+        v-for="user in users"
+        :key="user.userKey"
+        clickable
+        @click="() => selectMember(user)"
+      >
         <q-item-section avatar>
-          <img :src="user.avatarUrl">
+          <img :src="user.avatarUrl" />
         </q-item-section>
         <q-item-section>
           <q-item-label>{{ user.alias }}</q-item-label>
           <q-item-label caption lines="2">{{ user.userKey }}</q-item-label>
-          <q-item-label caption lines="3">{{ user.createdAt }}</q-item-label>          
+          <q-item-label caption lines="3">{{ user.createdAt }}</q-item-label>
         </q-item-section>
         <q-item-section side top>
           <q-item-label caption>{{ user.email }}</q-item-label>
-          <q-item-label v-for="role in user.roles" :key="role">{{ role }},</q-item-label>
+          <q-item-label v-for="role in user.roles" :key="role"
+            >{{ role }},</q-item-label
+          >
         </q-item-section>
       </q-item>
     </q-list>
+    <q-card v-if="selected" class="my-card bg-secondary text-white">
+      <q-card-section>
+        <div class="text-h6">{{ selected.alias }}</div>
+        <div class="text-subtitle2">member information</div>
+      </q-card-section>
+      <q-card-section>
+        Assigned Roles
+        <ul>
+          <li v-for="role in roles" :key="role">{{ role }}</li>
+        </ul>
+      </q-card-section>
+      <q-separator dark />
+      <q-card-actions>
+        <q-btn flat>Remove Role</q-btn>
+        <q-btn flat>Assign Role</q-btn>
+      </q-card-actions>
+    </q-card>
   </div>
 </template>
 
 <script>
-import { defineComponent, onMounted, ref} from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
 import { useStore, mapGetters } from 'vuex'
+import { fetchUserRoles } from '../../api/PowerUpApi'
 
 export default defineComponent({
   setup() {
@@ -39,17 +64,25 @@ export default defineComponent({
       store.dispatch('admin/refreshUsers')
     })
     return {
+      searchQuery: ref(null),
       showDetail: ref(false),
-      aliasToFind: ref(''),
+      selected: ref(null),
+      roles: ref([]),
     }
   },
   computed: {
-    ...mapGetters('admin', ['users'])
+    ...mapGetters('admin', ['users']),
   },
   methods: {
     search() {
-      console.log('you are looking for someone...who goes by...' + this.aliasToFind);
-    }
-  }
+      console.log('You are looking for', this.searchQuery)
+    },
+    async selectMember(member) {
+      this.selected = member
+      const roles = await fetchUserRoles(this.selected.userKey)
+      console.log('Found roles:', roles)
+      this.roles = roles
+    },
+  },
 })
 </script>
