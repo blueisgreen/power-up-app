7113a8cd9e665b11e04520a18af90e1504a59e16
Dave Mount <davemount@gmail.com>
2022-03-12 19:18:00 -0800

    Create a simplified view for casual user.

---
+++ src/layouts/SimpleLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header reveal elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          What say you?
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SimpleLayout',
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style></style>
---
+++ src/pages/SimpleStartPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page>
+    <div class="q-pa-md text-h1">Hello</div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ScratchPad',
+  setup() {
+    return {}
+  },
+  methods: {},
+})
+</script>
+
+<style></style>
