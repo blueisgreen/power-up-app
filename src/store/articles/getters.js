f28f6e175b5881a10773e6c53fdba94b022b26e3
Dave Mount <davemount@gmail.com>
2022-03-20 12:07:25 -0700

    Remove articles from vuex store

--- src/store/articles/getters.js
+++
@@ -1,8 +0,0 @@
-export const getArticle = (state) => (articleId) => {
-  console.log('get article', articleId)
-  return state.byId[articleId]
-}
-
-export function articles(state) {
-  return state.articleIds.map((id) => state.byId[id])
-}
