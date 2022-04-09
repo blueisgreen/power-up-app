b229aab78e1151fa73bc266ce1dec1a58cf93489
Dave Mount <davemount@gmail.com>
2022-04-09 14:15:10 -0700

    The remaining filters, plus lots of cleanup

--- src/pages/admin/SystemPanel.vue
+++ src/pages/admin/SystemPanel.vue
@@ -1,74 +1,102 @@
 <template>
   <div class="q-pa-md">
-    <h3>System Administration</h3>
+    <div>
+      <q-toolbar class="bg-primary glossy">
+        <q-toolbar-title shrink>When:</q-toolbar-title>
 
-    <q-toolbar class="bg-primary glossy">
-      <q-toolbar-title shrink>Filters:</q-toolbar-title>
+        <q-input
+          v-model="startFilter"
+          label="From"
+          label-color="orange-2"
+          filled
+        >
+          <template #prepend>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy
+                cover
+                transition-show="scale"
+                transition-hide="scale"
+              >
+                <q-date v-model="startFilter" mask="YYYY-MM-DD">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+        <q-input
+          v-model="endFilter"
+          label="Until"
+          label-color="orange-2"
+          filled
+        >
+          <template #prepend>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy
+                cover
+                transition-show="scale"
+                transition-hide="scale"
+              >
+                <q-date v-model="endFilter" mask="YYYY-MM-DD">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
 
-      <q-input v-model="startFilter" label="From" filled>
-        <template #prepend>
-          <q-icon name="event" class="cursor-pointer">
-            <q-popup-proxy
-              cover
-              transition-show="scale"
-              transition-hide="scale"
-            >
-              <q-date v-model="startFilter" mask="YYYY-MM-DD">
-                <div class="row items-center justify-end">
-                  <q-btn v-close-popup label="Close" color="primary" flat />
-                </div>
-              </q-date>
-            </q-popup-proxy>
-          </q-icon>
-        </template>
-      </q-input>
-      <q-input v-model="endFilter" label="Until" filled>
-        <template #prepend>
-          <q-icon name="event" class="cursor-pointer">
-            <q-popup-proxy
-              cover
-              transition-show="scale"
-              transition-hide="scale"
-            >
-              <q-date v-model="endFilter" mask="YYYY-MM-DD">
-                <div class="row items-center justify-end">
-                  <q-btn v-close-popup label="Close" color="primary" flat />
-                </div>
-              </q-date>
-            </q-popup-proxy>
-          </q-icon>
-        </template>
-      </q-input>
+        <q-space />
 
-      <q-select
-        v-model="limit"
-        :options="limitOptions"
-        outlined
-        label="Max Rows"
-        class="limit"
-      />
+        <q-select
+          v-model="limitFilter"
+          :options="limitOptions"
+          outlined
+          label="Rows"
+          label-color="orange-2"
+          class="limit"
+        />
+      </q-toolbar>
+      <q-toolbar class="bg-primary glossy">
+        <q-toolbar-title shrink>Focus on:</q-toolbar-title>
 
-      <q-btn icon="navigate_before" @click="prior" />
-      <q-btn icon="navigate_next" @click="next" />
-
-      <q-space></q-space>
-      <q-btn flat round dense icon="search" @click="findActionsUsingFilters" />
-    </q-toolbar>
-    <div class="row q-col-gutter-sm text-weight-bold bg-secondary">
-      <div class="col-2">When</div>
-      <div class="col-3">Who</div>
-      <div class="col-3">Action</div>
-      <div class="col-4">Details</div>
+        <q-input v-model="userKeyFilter" label="Who" label-color="orange-2" />
+        <q-input v-model="actionCodeFilter" label="Action" label-color="orange-2" />
+        <q-space />
+        <q-btn
+          flat
+          round
+          dense
+          icon="search"
+          @click="findActionsUsingFilters"
+        />
+        <span v-if="hasResults">
+          <q-btn icon="navigate_before" @click="prior" />
+          <q-btn icon="navigate_next" @click="next" />
+        </span>
+      </q-toolbar>
     </div>
-    <div
-      v-for="action in activity"
-      :key="action.createdAt"
-      class="row q-col-gutter-sm"
-    >
-      <div class="col-2">{{ formatDate(action.createdAt) }}</div>
-      <div class="col-3">{{ action.userKey }}</div>
-      <div class="col-3">{{ action.actionCode }}</div>
-      <div class="col-4">{{ action.details }}</div>
+    <div class="text-h5 text-center">Activity Records</div>
+    <div>
+      <div class="row q-col-gutter-sm text-weight-bold bg-secondary">
+        <div class="col-2">When</div>
+        <div class="col-3">Who</div>
+        <div class="col-3">Action</div>
+        <div class="col-4">Details</div>
+      </div>
+      <div
+        v-for="action in activity"
+        :key="action.createdAt"
+        class="row q-col-gutter-sm"
+      >
+        <div class="col-2">{{ formatDate(action.createdAt) }}</div>
+        <div class="col-3">{{ action.userKey }}</div>
+        <div class="col-3">{{ action.actionCode }}</div>
+        <div class="col-4">{{ action.details }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -81,20 +109,29 @@ export default defineComponent({
   setup() {
     const filterBuilder = actionFilterBuilder()
     const activity = ref([])
+    const userKeyFilter = ref('')
+    const actionCodeFilter = ref('')
     const limitOptions = [10, 25, 50, 75, 100]
-    const limit = ref(limitOptions[1])
+    const limitFilter = ref(limitOptions[1])
     const formatDate = (ts) => date.formatDate(ts, 'YYYY-MM-DD')
     const startFilter = ref()
     const endFilter = ref()
     return {
+      activity,
       formatDate,
+      filterBuilder,
+      userKeyFilter,
+      actionCodeFilter,
       startFilter,
       endFilter,
-      activity,
-      filterBuilder,
-      limit,
+      limitFilter,
       limitOptions,
     }
+  },
+  computed: {
+    hasResults() {
+      return this.activity.length > 0
+    },
   },
   methods: {
     async findActions() {
@@ -112,7 +149,13 @@ export default defineComponent({
     },
     async findActionsUsingFilters() {
       const builder = this.filterBuilder
-      builder.setLimit(this.limit)
+      builder.setLimit(this.limitFilter)
+      if (this.userKeyFilter && this.userKeyFilter.length > 0) {
+        builder.setUserKey(this.userKeyFilter)
+      }
+      if (this.actionCodeFilter && this.actionCodeFilter.length > 0) {
+        builder.setActionCode(this.actionCodeFilter)
+      }
       if (this.startFilter) {
         builder.setStart(this.startFilter)
       }
