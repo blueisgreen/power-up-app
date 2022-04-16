34c4ec4fa568a9cf3537fde4c93ada784fda38eb
Dave Mount <davemount@gmail.com>
2022-04-16 10:00:45 -0700

    Get ready to push

--- src/pages/whatsayyou/WhatSayYouPage.vue
+++ src/pages/whatsayyou/WhatSayYouPage.vue
@@ -1,12 +1,24 @@
 <template>
-  <q-page class="q-pa-md" style="max-width: 500px">
-    <q-input
-      v-model="prose"
-      autogrow
-      filled
-      label="Write something...anything."
-    />
-    <q-btn @click="save">Say It</q-btn>
+  <q-page class="q-pa-md">
+    <div class="row q-col-gutter-lg">
+      <div class="col" style="max-width: 500px">
+        <q-input
+          v-model="prose"
+          autogrow
+          filled
+          label="Write something...anything."
+        />
+        <q-btn @click="save">Say It</q-btn>
+      </div>
+      <div class="col">
+        <div class="text-h2">What You've Said</div>
+        <div class="">{{ lastSavePoint }}</div>
+        <hr />
+        <div><span v-html="lastSavePoint" /></div>
+        <hr />
+        <div><span v-html="afterMassaging" /></div>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -16,14 +28,23 @@ import { defineComponent, ref } from 'vue'
 export default defineComponent({
   name: 'WhatSayYouPage',
   setup() {
-    const prose = ref('')
+    const prose = ref(
+      '<p>this is exactly the kind of thing...</p><p>we should tweet about</p>'
+    )
+    const lastSavePoint = ref('')
     return {
       prose,
+      lastSavePoint,
     }
+  },
+  computed: {
+    afterMassaging() {
+      return this.lastSavePoint
+    },
   },
   methods: {
     save() {
-      alert('You said:' + this.prose)
+      this.lastSavePoint = this.prose
     },
   },
 })
