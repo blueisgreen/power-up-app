896b3dd89bd60368896d7c264715ed861968719d
Dave Mount <davemount@gmail.com>
2022-05-15 18:00:06 -0700

    Reducing effort to join.

--- src/pages/account/MemberRegistrationPage.vue
+++ src/pages/account/MemberRegistrationPage.vue
@@ -1,16 +1,69 @@
 <template>
   <div>
-    <registration-form />
+    <p class="text-h3">Set Up Account</p>
+    <q-form greedy @submit="onSubmit">
+      <q-input
+        v-model="formValues.screenName"
+        outlined
+        label="Screen Name"
+        hint="This is how you would like to be know within the Power Up community. Use an alias, a nickname, your given name, anything you like."
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+      />
+      <q-toggle
+        v-model="formValues.termsModel"
+        label="I agree to the terms of use."
+      />
+      <q-toggle
+        v-model="formValues.cookiesModel"
+        label="Cookies are fine by me."
+      />
+      <p>
+        Power Up Magazine uses cookies for your benefit when using the website.
+      </p>
+      <q-btn label="Power Up! (Join)" type="submit" color="primary" />
+    </q-form>
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import RegistrationForm from 'src/pages/account/RegistrationForm.vue'
+import { useQuasar } from 'quasar'
+import { defineComponent, ref } from 'vue'
+
+// TODO: call API to join as a member
 
 export default defineComponent({
-  components: {
-    RegistrationForm
+  setup() {
+    const $q = useQuasar()
+    const formValues = ref({
+      screenName: '',
+      termsModel: false,
+      cookiesModel: false,
+    })
+    return {
+      formValues,
+      $q,
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (this.formValues.termsModel !== true) {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'You must accept the terms to join.',
+        })
+      } else {
+        console.log('you are joining - except it does not work yet')
+        this.$q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'cloud_done',
+          message: 'Your application was submitted. Good luck!',
+        })
+      }
+    },
   },
 })
 </script>
